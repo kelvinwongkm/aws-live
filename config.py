496ca950c680e75e4d7d33b2c68f62eed8973cee
Kelvin Wong <70678114+kelvinwongkm@users.noreply.github.com>
2023-08-29 16:32:36 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.cbjxe9hi8rvj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongkahming-employee"
+customregion = "us-east-1"
+
